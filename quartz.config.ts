fff00f0b99ac862bfbaf2d02dc0e6542130f1eee
nabilbinsayed <nabilprometheus@gmail.com>
2026-03-11 09:33:32 +0600

    Quartz sync: Mar 11, 2026, 9:33 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -23,9 +23,9 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Schibsted Grotesk",
-        body: "Source Sans Pro",
-        code: "IBM Plex Mono",
+        header: "Outfit",
+        body: "Lora",
+        code: "JetBrains Mono",
       },
       colors: {
         lightMode: {
